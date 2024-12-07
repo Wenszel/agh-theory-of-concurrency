7dcea0cc3c6afe8c34b1368bae61fb0f17f059ea
Wenszel <wiktor.smaga13@gmail.com>
2024-12-07 21:13:05 +0100

    added graph generator

--- zad2/src/main/java/org/example/Main.java
+++ zad2/src/main/java/org/example/Main.java
@@ -1,15 +1,35 @@
 package org.example;
 
+import guru.nidi.graphviz.attribute.Label;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.MutableGraph;
+import guru.nidi.graphviz.model.MutableNode;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
+import static guru.nidi.graphviz.model.Factory.mutGraph;
+import static guru.nidi.graphviz.model.Factory.mutNode;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Map<Character, Integer> variables = new HashMap<>(Map.of('x', 5, 'y', 10, 'z', 7));
         List<Character> A = List.of('a', 'b', 'c', 'd');
         List<String> operationsInput = List.of("x = x + y", "y = y + 2z", "x = 3x + z", "z = y - z");
+        char[] word = "baadcb".toCharArray();
+        run(variables, A, operationsInput, word);
 
+//        variables = new HashMap<>(Map.of('x', 5, 'y', 10, 'z', 7, 'w', 3, 'v', 2)); A = List.of('a', 'b', 'c', 'd', 'e', 'f');
+//        operationsInput = List.of("x = x + 1", "y = y + 2z", "x = 3x + z", "w = w + v", "z = y - z", "v = x + v");
+//        word = "acdcfbbe".toCharArray();
+//        run(variables, A, operationsInput, word);
+    }
+
+    public static void run(Map<Character, Integer> variables, List<Character> A, List<String> operationsInput, char[] word) throws IOException {
         Map<Character, Operation> availableOperations = new HashMap<>();
-
         for (int i = 0; i < operationsInput.size(); i++) {
             availableOperations.put(A.get(i), new Operation(A.get(i), operationsInput.get(i), variables));
         }
@@ -29,9 +49,11 @@ public class Main {
         System.out.println("D = {" + dependencies + "}");
         System.out.println("I = {" + independencies + "}");
 
-
-        char[] word = "baadcb".toCharArray();
-        System.out.println(calculateFoataClasses(word, dependencies));
+        List<List<Character>> FNF = calculateFoataClasses(word, dependencies);
+        System.out.println(FNF);
+        MutableGraph graph = generateFoataGraph(FNF, dependencies);
+        Graphviz.fromGraph(graph).render(Format.PNG).toFile(new File("foata-graph.png"));
+        System.out.println("The graph has been generated as foata-graph.png");
     }
 
     private static List<List<Character>> calculateFoataClasses(char[] word, List<Pair<Character>> dependencies) {
@@ -70,5 +92,92 @@ public class Main {
             }
         }
         return true;
+    }
+
+    private static MutableGraph generateFoataGraph(List<List<Character>> fnf, List<Pair<Character>> dependencies) {
+        MutableGraph graph = mutGraph("Foata Graph").setDirected(true);
+        Map<MyNode, Character> nodes = new HashMap<>();
+        List<List<MyNode>> nodesInClasses = new ArrayList<>();
+
+        for (int i = 0; i < fnf.size(); i++) {
+            nodesInClasses.add(new ArrayList<>());
+        }
+
+        int index = 0;
+
+        for (int classIndex = 0; classIndex < fnf.size(); classIndex++) {
+            List<Character> foataClass = fnf.get(classIndex);
+            for (char el : foataClass) {
+                MutableNode node = mutNode(String.valueOf(index)).add(Label.of(String.valueOf(el)));
+                MyNode myNode = new MyNode(index, el, node, false);
+                nodesInClasses.get(classIndex).add(myNode);
+                nodes.put(myNode, el);
+                graph.add(node);
+                index++;
+            }
+        }
+
+        for (int classIndex = 1; classIndex < nodesInClasses.size(); classIndex++) {
+            for (MyNode currentNode : nodesInClasses.get(classIndex)) {
+                Character currentChar = nodes.get(currentNode);
+                boolean flag = true;
+                for (int olderClassIndex = classIndex - 1; olderClassIndex >= 0; olderClassIndex--) {
+                    for (MyNode olderNode: nodesInClasses.get(olderClassIndex)) {
+                        Character olderChar = nodes.get(olderNode);
+                        if (isDependent(olderChar, currentChar, dependencies) && (!olderNode.haveEdge() || flag) ) {
+                            System.out.println("Adding edge from " + olderChar + " to " + currentChar);
+                            olderNode.node().addLink(currentNode.node());
+                            olderNode.setHaveEdge(true);
+                            flag = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return graph;
+    }
+
+    // Funkcja pomocnicza sprawdzająca, czy dwie zmienne są zależne na podstawie listy dependencies
+    private static boolean isDependent(char from, char to, List<Pair<Character>> dependencies) {
+        return dependencies.contains(new Pair<>(from, to));
+    }
+
+    private static class MyNode {
+        private final int index;
+        private final char value;
+        private final MutableNode node;
+        private boolean haveEdge;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MyNode myNode = (MyNode) o;
+            return index == myNode.index && value == myNode.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(index, value);
+        }
+
+        public MyNode(int index, char value, MutableNode node, boolean haveEdge) {
+            this.index = index;
+            this.value = value;
+            this.node = node;
+            this.haveEdge = haveEdge;
+        }
+        public MutableNode node() {
+            return node;
+        }
+
+        public boolean haveEdge() {
+            return haveEdge;
+        }
+
+        public void setHaveEdge(boolean haveEdge) {
+            this.haveEdge = haveEdge;
+        }
+
     }
 }
--- zad2/src/main/java/org/example/Operation.java
+++ zad2/src/main/java/org/example/Operation.java
@@ -7,7 +7,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Operation implements Runnable {
-    public static final String OPERATION_REGEX = "([a-z])\\s*=\\s*(\\d*)([a-z])\\s*([*\\-+])\\s*(\\d*)([a-z])";
+    public static final String OPERATION_REGEX = "([a-z])\\s*=\\s*(\\d*)([a-z]*)\\s*([*\\-+])\\s*(\\d*)([a-z]*)";
+    public static final char NO_VARIABLE = '-';
     private final char symbol;
     private final char varToModify;
     private Runnable operation;
@@ -23,10 +24,10 @@ public class Operation implements Runnable {
 
         varToModify = matcher.group(1).charAt(0);
         int multiplier1 = matcher.group(2).isEmpty() ? 1 : Integer.parseInt(matcher.group(2));
-        char var1 = matcher.group(3).charAt(0);
+        char var1 =  matcher.group(3).isEmpty() ? NO_VARIABLE : matcher.group(3).charAt(0);
         char operator = matcher.group(4).charAt(0);
         int multiplier2 = matcher.group(5).isEmpty() ? 1 : Integer.parseInt(matcher.group(5));
-        char var2 = matcher.group(6).charAt(0);
+        char var2 = matcher.group(6).isEmpty() ? NO_VARIABLE : matcher.group(6).charAt(0);
 
         initVariablesUsedInOperation(var1, var2, varToModify);
         initOperation(variables, multiplier1, var1, multiplier2, var2, operator, varToModify);
@@ -34,8 +35,8 @@ public class Operation implements Runnable {
 
     private void initOperation(Map<Character, Integer> variables, int multiplier1, char var1, int multiplier2, char var2, char operator, char varToModify) {
         operation = () -> {
-            int value1 = multiplier1 * variables.get(var1);
-            int value2 = multiplier2 * variables.get(var2);
+            int value1 = multiplier1 * variables.getOrDefault(var1, 1);
+            int value2 = multiplier2 * variables.getOrDefault(var2, 1);
             int result = switch (operator) {
                 case '+' -> value1 + value2;
                 case '-' -> value1 - value2;
